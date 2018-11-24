f7e20d8d670c32798baa75016ee0c89cb6dd2960
8845musign <8845musign@gmail.com>
2018-11-25 02:08:44 +0900

    imple navigater

---
+++ src/components/Chat.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="left">
+        <v-ons-back-button>BACK</v-ons-back-button>
+      </div>
+      <div class="center">CHAT</div>
+    </v-ons-toolbar>
+  </v-ons-page>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template id="home">
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="center">HOME</div>
+    </v-ons-toolbar>
+
+    <div class="content">
+      <v-ons-button @click="push">PUSH</v-ons-button>
+    </div>
+  </v-ons-page>
+</template>
+
+<script>
+import Chat from './Chat'
+
+export default {
+  methods: {
+    push () {
+      this.$emit('push-page', Chat)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
